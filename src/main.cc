0ec836364919afc9e4fe3e7665f70fe564deb4f9
VC Science Labs <vscilab@vcccd.edu>
2018-09-13 09:46:31 -0700

    Hey, I did it...probably

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+/**
+* CS V13 Object-Oriented Programming
+* CRN: Some Value
+* Assignment: EX01-HelloWorld
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source. (Except from the Professor himself, cause this is friggin "Hello World")
+*
+* @author Tristan Long
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {	
+
+	cout << "Hello, World!" << endl;
+
+return 0;
+}
